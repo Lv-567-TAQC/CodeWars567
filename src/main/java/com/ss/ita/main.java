3a1b99ecc4a3409c8a9a4fdce6e8aea36dac5ca5
Liubomyr Halamaha <galamagal86@gmail.com>
2021-01-21 12:55:46 +0200

    Create main.java

---
+++ src/main/java/com/ss/ita/main.java
@@ -0,0 +1,11 @@
+package com.ss.ita;
+
+import com.ss.ita.menu.Menu;
+import com.ss.ita.util.implementation.ConsoleScanner;
+
+public class main {
+    public static void main(String[] args) {
+       Menu menu = new Menu();
+       menu.mainMenu();
+    }
+}
