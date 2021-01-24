e7a7cbbbce7a8c413613dbd16942a2e6e393ed89
Khrystyna Shalavylo <shalavilo52@gmail.com>
2021-01-24 18:06:38 +0200

    added Volume of Cuboid

---
+++ src/main/java/com/ss/ita/kata/khrystiash/VolumeOfCuboid.java
@@ -0,0 +1,52 @@
+package com.ss.ita.kata.khrystiash;
+
+import com.ss.ita.kata.eight;
+
+public class VolumeOfCuboid implements eight {
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        double volumeOfCuboid;
+        volumeOfCuboid = length * width * height;
+        return volumeOfCuboid;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
