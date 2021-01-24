586f9cf61175546f75b6b0a1db26eb17e9621e38
Nataliia Koval <natalia.koval23@gmail.com>
2021-01-24 17:28:41 +0200

    Koval_VolumeOfACuboid

---
+++ src/com/ss/ita/kata/VolumeOfACuboid.java
@@ -0,0 +1,10 @@
+package com.ss.ita.kata;
+
+public class VolumeOfACuboid implements Eight {
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+
+        return length * width * height;
+    }
+}
