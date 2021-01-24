eded2cf7ac3050fef3b8c7bc2eeb485a33cbaedf
Roman-TAQC <roman.it@gmail.com>
2021-01-24 04:05:30 +0200

    Added "Count of positives / sum of negatives" kata

---
+++ src/main/java/com/ss/ita/kata/CountPositivesSumNegatives.java
@@ -0,0 +1,24 @@
+package com.ss.ita.kata;
+
+public class CountPositivesSumNegatives implements Eight{
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        int positiveCount =  0;
+        int negativeSum = 0;
+
+        if (input == null || input.length == 0) {
+            int[] arr = {};
+            return arr;
+        }
+        for(int num: input){
+            if(num >= 0){
+                positiveCount++;
+            }
+            else {
+                negativeSum += num;
+            }
+        }
+        return new int[]{positiveCount, negativeSum};
+    }
+}
