2edcbd3c97b1a2a6aaacd4cbf198f1243557b929
Nataliia Koval <natalia.koval23@gmail.com>
2021-01-24 19:52:51 +0200

    Koval_CountOfPositivesSumOfNegatives

---
+++ src/com/ss/ita/kata/CountOfPositivesSumOfNegatives.java
@@ -0,0 +1,20 @@
+package com.ss.ita.kata;
+
+public class CountOfPositivesSumOfNegatives {
+    public static int[] countPositivesSumNegatives(int[] input)
+    {
+        int [] array = new int [2];
+        if(input.length != 0 || input != null){
+            for(int i = 0; i <= input.length - 1 ; i++){
+                if(input[i] > 0){
+                    array[0]++;
+                }else{
+                    array[1] += input[i];
+                }
+            }
+            return array;
+        }else{
+            return input;
+        }
+    }
+}
