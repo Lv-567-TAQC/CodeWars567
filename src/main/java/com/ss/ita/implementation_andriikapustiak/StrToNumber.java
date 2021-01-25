4f29d10f5ee0465adbccc6bf5511a1ee2d5f27af
Andrii Kapustiak <andriikapustiak@gmail.com>
2021-01-25 18:01:52 +0200

    String to number

---
+++ src/main/java/com/ss/ita/implementation_andriikapustiak/StrToNumber.java
@@ -0,0 +1,8 @@
+package com.ss.ita.implementation_andriikapustiak;
+
+public class StrToNumber {
+    public static int stringToNumber(String str) {
+        //TODO: Convert str into a number
+        return Integer.parseInt(str);
+    }
+}
