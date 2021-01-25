a27dd580162d3aa1040bef6ed21eff97576c64a4
I T <47854554+Nnn7@users.noreply.github.com>
2021-01-25 20:51:46 +0200

    Update KeepHydrated.java

--- src/main/java/com/ss/ita/kata/KeepHydrated.java
+++ src/main/java/com/ss/ita/kata/KeepHydrated.java
@@ -5,10 +5,11 @@ public class KeepHydrated implements Eight{
 	
 	  public int liters(double time)  {
 		    
-			double liters = 0.5f;
-			liters=(liters*time) - (liters*time)%1;
-			//System.out.println(liters);
-			int result=(int)liters; 
+		double liters = 0.5f;
+		liters=(liters*time) - (liters*time)%1;
+			
+		//System.out.println(liters);
+		int result=(int)liters; 
 	    	return result;
 	  
 	  }
