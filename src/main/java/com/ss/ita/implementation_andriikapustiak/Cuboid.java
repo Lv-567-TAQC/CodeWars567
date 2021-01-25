6168a4e88daf4077a63241089b9f5580eaab374d
Andrii Kapustiak <andriikapustiak@gmail.com>
2021-01-25 15:16:12 +0200

    Volume of Cuboid

---
+++ src/main/java/com/ss/ita/implementation_andriikapustiak/Cuboid.java
@@ -0,0 +1,14 @@
+package com.ss.ita.implementation_andriikapustiak;
+
+public class Cuboid {
+
+    static void calcTtl(int l, int w, int h){
+        int s = l * w * h;
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        calcTtl (3,5,6);
+    }
+
+}
