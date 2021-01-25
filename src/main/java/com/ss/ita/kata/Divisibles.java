fd2629ad0a0ddea7a6d98267841e45269f90f358
Roman-TAQC <roman.it@gmail.com>
2021-01-25 12:46:58 +0200

    Added "Find numbers which are divisible by given number" kata

---
+++ src/main/java/com/ss/ita/kata/Divisibles.java
@@ -0,0 +1,25 @@
+package com.ss.ita.kata;
+
+public class Divisibles implements Eight{
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        int i = 0;
+
+        for (int num: numbers){
+            if (num %  divider == 0){
+                i++;
+            }
+        }
+
+        int[] divisibleNumbers = new int[i];
+        i = 0;
+
+        for (int num: numbers){
+            if (num %  divider == 0){
+                divisibleNumbers[i++] = num;
+            }
+        }
+        return divisibleNumbers;
+    }
+}
+
