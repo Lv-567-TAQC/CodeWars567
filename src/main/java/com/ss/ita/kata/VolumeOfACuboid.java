b163a5b05850a68fc17a20bc1e15d58124586f08
Roman-TAQC <roman.it@gmail.com>
2021-01-25 12:22:08 +0200

    Added "Volume of a Cuboid" kata

---
+++ src/main/java/com/ss/ita/kata/VolumeOfACuboid.java
@@ -0,0 +1,8 @@
+package com.ss.ita.kata;
+
+public class VolumeOfACuboid implements Eight{
+    public double getVolumeOfCuboid(final double length, final double width, final double height) {
+        double volume = length * width * height;
+        return volume;
+    }
+}
