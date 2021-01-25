c0a561fa324bba2f00ccc37b35d6560679d72b0b
Andrii Kapustiak <andriikapustiak@gmail.com>
2021-01-25 15:47:18 +0200

    decimal places

---
+++ src/main/java/com/ss/ita/implementation_andriikapustiak/Decimals.java
@@ -0,0 +1,9 @@
+package com.ss.ita.implementation_andriikapustiak;
+
+public class Decimals {
+    public static double TwoDecimalPlaces(double number)
+    {
+        return Math.round(number * 100.0) / 100.0;
+
+    }
+}
