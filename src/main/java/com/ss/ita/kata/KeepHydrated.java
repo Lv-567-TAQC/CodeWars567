d616249eeabf58db764beaa6817573bbedce0cfa
Nnn7 <nsteronua@gmail.com>
2021-01-25 20:37:38 +0200

    KeepHydrated was rebuilded

---
+++ src/main/java/com/ss/ita/kata/KeepHydrated.java
@@ -0,0 +1,16 @@
+package com.ss.ita.kata;
+
+
+public class KeepHydrated implements Eight{
+	
+	  public int liters(double time)  {
+		    
+			double liters = 0.5f;
+			liters=(liters*time) - (liters*time)%1;
+			//System.out.println(liters);
+			int result=(int)liters; 
+	    	return result;
+	  
+	  }
+
+}
