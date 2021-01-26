cbf7988605dde9e0758454426e9f1f3a3661e7c5
Roman-TAQC <roman.it@gmail.com>
2021-01-26 17:28:18 +0200

    Added 'Count of positives / sum of negatives'

---
+++ src/main/java/com/ss/ita/kata/implementation/romankhvalbota/EightImpl.java
@@ -0,0 +1,65 @@
+package com.ss.ita.kata.implementation.romankhvalbota;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        int positiveCount = 0;
+        int negativeSum = 0;
+
+        if (input == null || input.length == 0){
+            int[] arr = {};
+            return arr;
+        }
+        for(int num : input){
+            if(num > 0){
+                positiveCount++;
+            }
+            else {
+                negativeSum += num;
+            }
+        }
+        return new int[]{positiveCount, negativeSum};
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
