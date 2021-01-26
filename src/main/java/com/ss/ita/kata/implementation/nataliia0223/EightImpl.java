734be35975cd91469a3787f934a8ca8e8ff3a256
Nataliia Koval <natalia.koval23@gmail.com>
2021-01-26 19:31:50 +0200

    nataliia0223/CountOfPositivesSumOfNegatives

--- src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
@@ -5,14 +5,11 @@ import com.ss.ita.kata.Eight;
 public class EightImpl implements Eight {
 
     @Override
-    public  int liters(double time){
-        int liters = (int) time / 2;
-        return liters;
-    }
+    public  int liters(double time){ return 0; }
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return length * width * height;
+        return 0;
     }
 
     @Override
@@ -27,7 +24,19 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        int [] array = new int [2];
+        if(input.length != 0 || input != null){
+            for(int i = 0; i <= input.length - 1 ; i++){
+                if(input[i] > 0){
+                    array[0]++;
+                }else{
+                    array[1] += input[i];
+                }
+            }
+            return array;
+        }else{
+            return input;
+        }
     }
 
     @Override
