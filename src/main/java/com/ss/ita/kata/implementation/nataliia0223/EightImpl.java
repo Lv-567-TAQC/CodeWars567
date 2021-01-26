c56414304afba4a31bc2375714e478180e8cbb4d
Nataliia Koval <natalia.koval23@gmail.com>
2021-01-26 19:11:09 +0200

    nataliia0223/MilesPerGallonToKilometersPerLiter

--- src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
@@ -16,8 +16,9 @@ public class EightImpl implements Eight {
     }
 
     @Override
-    public float mpgToKPM(float mpg) {
-        return 0;
+    public static float mpgToKPM(final float mpg) {
+        float kpl = (float)1.609344 / (float)4.54609188 * mpg;
+        return kpl;
     }
 
     @Override
