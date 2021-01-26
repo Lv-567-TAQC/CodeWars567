0dd18ad7ff2034e093605e29d498eb7ce53b8861
Nataliia Koval <natalia.koval23@gmail.com>
2021-01-26 19:51:55 +0200

    SevenImpl

---
+++ src/main/java/com/ss/ita/kata/implementation/nataliia0223/SevenImpl.java
@@ -0,0 +1,21 @@
+package com.ss.ita.kata.implementation.nataliia0223;
+
+import com.ss.ita.kata.Seven;
+
+public class SevenImpl implements Seven {
+
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        return 0;
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        return null;
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return 0;
+    }
+}
