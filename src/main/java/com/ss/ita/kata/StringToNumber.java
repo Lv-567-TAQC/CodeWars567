d2dc13140b290e3a56a41894fc1d1fbf47db7635
Roman-TAQC <roman.it@gmail.com>
2021-01-26 02:42:15 +0200

    Added "Convert a String to a Number!"

---
+++ src/main/java/com/ss/ita/kata/StringToNumber.java
@@ -0,0 +1,8 @@
+package com.ss.ita.kata;
+
+public class StringToNumber implements Eight{
+    @Override
+    public int stringToNumber(String str) {
+        return Integer.parseInt(str);
+    }
+}
