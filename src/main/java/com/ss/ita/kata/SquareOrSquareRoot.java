ec99397d38a4f26e6cca0c64110b6112d4b55399
Roman-TAQC <roman.it@gmail.com>
2021-01-26 02:53:00 +0200

    Added "To square(root) or not to square(root)"

---
+++ src/main/java/com/ss/ita/kata/SquareOrSquareRoot.java
@@ -0,0 +1,22 @@
+package com.ss.ita.kata;
+
+public class SquareOrSquareRoot implements Eight{
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        int[] changed = new int[array.length];
+        int i = 0;
+        int isSquare;
+
+        for (int num : array) {
+            isSquare = (int) Math.sqrt(num);
+
+            if (isSquare * isSquare == num){
+                changed[i++] = isSquare;
+            }
+            else{
+                changed[i++] = num * num;
+            }
+        }
+        return changed;
+    }
+}
