5ee54b1855f4e4102f5b106a8ec3b660964f40c7
Khrystyna Shalavylo <shalavilo52@gmail.com>
2021-01-26 18:30:17 +0200

    added Wilson Primes

--- src/main/java/com/ss/ita/kata/implementation/khrystiash/EightImplementation.java
+++ src/main/java/com/ss/ita/kata/implementation/khrystiash/EightImplementation.java
@@ -85,7 +85,39 @@ public class EightImplementation implements Eight {
 
     @Override
     public boolean amIWilson(double n) {
-        return false;
+        boolean isWilsonNumber = isNumberPrime(n);
+        if(isWilsonNumber){
+            isWilsonNumber = (wilsonNumberCalculation(n))
+                    - (int)wilsonNumberCalculation(n) == 0;
+        }
+        return isWilsonNumber;
+    }
+
+    boolean isNumberPrime(double n){
+        boolean isPrime = false;
+        for (int i = 2; i < n; i++){
+            if ((n / i) - (int) (n / i) == 0) {
+                isPrime = false;
+                break;
+            }else{
+                isPrime = true;
+            }
+        }
+        return isPrime;
+    }
+
+    double factorial(double number){
+        double numberFactorial=1;
+        for (int i = 0;i < number;i++){
+            numberFactorial *= number-i;
+        }
+        return numberFactorial;
+    }
+
+    double wilsonNumberCalculation(double number){
+        double wilsonNumber;
+        wilsonNumber = (factorial(number - 1) + 1) / (number * number);
+        return wilsonNumber;
     }
 
     @Override
