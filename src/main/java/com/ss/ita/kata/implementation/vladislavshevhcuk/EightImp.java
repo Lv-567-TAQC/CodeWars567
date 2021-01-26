42d54ed758253706ceec68f0c47c17a6e00087a7
c1nnamon-ro11 <vladislaw.vlad2@gmail.com>
2021-01-27 00:44:54 +0200

    c1nnamon-ro11/Convert a String to a Number!

--- src/main/java/com/ss/ita/kata/implementation/vladislavshevhcuk/EightImp.java
+++ src/main/java/com/ss/ita/kata/implementation/vladislavshevhcuk/EightImp.java
@@ -57,7 +57,7 @@ public class EightImp implements Eight {
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+        return Integer.parseInt(str);
     }
 
     @Override
