b551a00e2ee283646b8ac999ac33f7cec049d875
c1nnamon-ro11 <vladislaw.vlad2@gmail.com>
2021-01-27 00:37:21 +0200

    c1nnamon-ro11/To square(root) or not to square(root)

--- src/main/java/com/ss/ita/kata/implementation/vladislavshevhcuk/EightImp.java
+++ src/main/java/com/ss/ita/kata/implementation/vladislavshevhcuk/EightImp.java
@@ -18,12 +18,21 @@ public class EightImp implements Eight {
 
     @Override
     public float mpgToKPM(float mpg) {
-        return 0;
+        if (mpg > 0) {
+            final double IMPERIAL_GALLON = 4.54609188;
+            final double MILE = 1.609344;
+            return (float) (Math.round(mpg * MILE / IMPERIAL_GALLON * 100) / 100.00);
+        }
+        return -1;
     }
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.sqrt(array[i]) % 1 == 0 ? (int) Math.sqrt(array[i]) : (int) Math.pow(array[i], 2);
+        }
+        return array;
     }
 
     @Override
