4f3dd479a75f82694a43c60f38ed3fdfd39623b2
Roman-TAQC <roman.it@gmail.com>
2021-01-27 05:38:52 +0200

    Added 'Find numbers which are divisible by given number'

--- src/main/java/com/ss/ita/kata/implementation/romankhvalbota/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/romankhvalbota/EightImpl.java
@@ -80,6 +80,22 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        int i = 0;
+
+        for (int num: numbers){
+            if (num %  divider == 0){
+                i++;
+            }
+        }
+
+        int[] divisibleNumbers = new int[i];
+        i = 0;
+
+        for (int num: numbers){
+            if (num %  divider == 0){
+                divisibleNumbers[i++] = num;
+            }
+        }
+        return divisibleNumbers;
     }
 }
