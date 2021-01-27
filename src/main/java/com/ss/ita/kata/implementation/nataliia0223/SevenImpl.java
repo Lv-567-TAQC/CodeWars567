9939c1b89479a192b9592488f85e9b4e6ba22a60
Nataliia Koval <natalia.koval23@gmail.com>
2021-01-27 21:22:38 +0200

    nataliia0223/SumOfTheFirstNthTermOfSeries

---
+++ src/main/java/com/ss/ita/kata/implementation/nataliia0223/SevenImpl.java
@@ -0,0 +1,29 @@
+package com.ss.ita.kata.implementation.nataliia0223;
+
+import com.ss.ita.kata.Seven;
+
+public class SevenImpl implements Seven {
+
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        return 0;
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        double sum = 0;
+        double k = 1.0;
+        for(int i = 1; i <= n; i++){
+            sum += (1 / k);
+            k += 3;
+        }
+        sum = (double)Math.round(sum * 100) / 100;
+
+        return Double.toString(sum);
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return 0;
+    }
+}
