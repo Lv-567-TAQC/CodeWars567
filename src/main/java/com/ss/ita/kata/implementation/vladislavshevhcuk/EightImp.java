0901f211be9fa1a7d61a2534d6eb7bac0e5538f7
Vlad Shevchuk <71972963+c1nnamon-ro11@users.noreply.github.com>
2021-01-27 15:47:08 +0200

    C1nnamon roll/ranking nba teams (#91)
    * c1nnamon-ro11/getVolumeOfCuboid
    
    * c1nnamon-ro11/To square(root) or not to square(root)
    
    * c1nnamon-ro11/Count of positives / sum of negatives
    
    * c1nnamon-ro11/Convert a String to a Number!
    
    * c1nnamon-ro11/Wilson primes
    
    * c1nnamon-ro11/Formatting decimal places #0
    
    * c1nnamon-ro11/Find numbers which are divisible by given number
    
    * c1nnamon-ro11/Looking for a benefactor
    
    * c1nnamon-ro11/Sum of the first nth term of Series
    
    * c1nnamon-ro11/Where is Vasya?
    
    * c1nnamon-ro11/Build a pile of Cubes
    
    * c1nnamon-ro11/Easy Balance Checking
    
    * c1nnamon-ro11/Rainfall
    
    * c1nnamon-ro11/Ranking NBA teams

--- src/main/java/com/ss/ita/kata/implementation/vladislavshevhcuk/EightImp.java
+++ src/main/java/com/ss/ita/kata/implementation/vladislavshevhcuk/EightImp.java
@@ -1,6 +1,7 @@
 package com.ss.ita.kata.implementation.vladislavshevhcuk;
 
 import com.ss.ita.kata.Eight;
+import java.util.ArrayList;
 
 public class EightImp implements Eight {
 
@@ -11,41 +12,83 @@ public class EightImp implements Eight {
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return 0;
+        if (length > 0 && width > 0 && height > 0)
+            return length * width * height;
+        return -1;
     }
 
     @Override
     public float mpgToKPM(float mpg) {
-        return 0;
+        if (mpg > 0) {
+            final double IMPERIAL_GALLON = 4.54609188;
+            final double MILE = 1.609344;
+            return (float) (Math.round(mpg * MILE / IMPERIAL_GALLON * 100) / 100.00);
+        }
+        return -1;
     }
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.sqrt(array[i]) % 1 == 0 ? (int) Math.sqrt(array[i]) : (int) Math.pow(array[i], 2);
+        }
+        return array;
     }
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+
+        if(input == null || input.length==0){
+            int[] emptyArray = {};
+            return emptyArray;
+        }
+        int[] result = new int[2];
+        int countOfPositiveNumbers =0;
+        int sumOfNegativeNumbers = 0;
+        for(int element : input){
+            if(element>0)
+                countOfPositiveNumbers++;
+            else sumOfNegativeNumbers+=element;
+        }
+        result[0]=countOfPositiveNumbers;
+        result[1]=sumOfNegativeNumbers;
+        return result; //return an array with count of positives and sum of negatives
     }
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+        return Integer.parseInt(str);
     }
 
     @Override
     public boolean amIWilson(double n) {
-        return false;
+        double mod = n*n;
+        double fact = 1;
+        for(int i=2;i<n;i++){
+            fact = (fact*i)%mod;
+        }
+        return fact+1 == mod;
     }
 
     @Override
     public double twoDecimalPlaces(double number) {
-        return 0;
+        //number = Math.round(number*100)/100.00;
+        number = Double.parseDouble(String.format("%.2f", number));
+        return number;
     }
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int number : numbers){
+            if(number%divider == 0){ list.add(number);}
+        }
+
+        int[] result = new int[list.size()];
+        for(int i=0;i<result.length;i++){
+            result[i] = list.get(i);
+        }
+        return result;
     }
 }
