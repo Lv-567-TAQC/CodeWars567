22167576434cfad548a1eedf4abdc62ba39d349d
Nnn7 <nsteronua@gmail.com>
2021-01-27 13:47:37 +0200

    Bug fix v2

---
+++ src/main/java/com/ss/ita/kata/Implements/EightImpl.java
@@ -0,0 +1,64 @@
+package com.ss.ita.kata.Implements;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight{
+
+	@Override
+	public int liters(double time) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getVolumeOfCuboid(double length, double width, double height) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float mpgToKPM(float mpg) {
+		double kmperlitres;
+		double changer = 1.609344d/ 4.54609188;
+		kmperlitres= mpg*changer;
+   		double result = Math.round(kmperlitres*100)/100.00;
+    	return  (float)result;
+	}
+
+	@Override
+	public int[] squareOrSquareRoot(int[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] countPositivesSumNegatives(int[] input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int stringToNumber(String str) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean amIWilson(double n) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double twoDecimalPlaces(double number) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] divisibleBy(int[] numbers, int divider) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
