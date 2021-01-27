de8e568290b3b99ba4b853977c638b8553e4735b
Roman-TAQC <roman.it@gmail.com>
2021-01-27 05:31:17 +0200

    Added 'To square(root) or not to square(root)'

--- src/main/java/com/ss/ita/kata/implementation/romankhvalbota/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/romankhvalbota/EightImpl.java
@@ -22,7 +22,21 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        int[] changed = new int[array.length];
+        int i = 0;
+        int isSquare;
+
+        for (int num : array) {
+            isSquare = (int) Math.sqrt(num);
+
+            if (isSquare * isSquare == num){
+                changed[i++] = isSquare;
+            }
+            else{
+                changed[i++] = num * num;
+            }
+        }
+        return changed;
     }
 
     @Override
