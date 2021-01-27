c1f3f8e5a108d6ea942cdb6f8fe53c681e74482b
Roman-TAQC <roman.it@gmail.com>
2021-01-27 05:35:24 +0200

    Added 'Miles per gallon to kilometers per liter'

--- src/main/java/com/ss/ita/kata/implementation/romankhvalbota/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/romankhvalbota/EightImpl.java
@@ -17,7 +17,9 @@ public class EightImpl implements Eight {
 
     @Override
     public float mpgToKPM(float mpg) {
-        return 0;
+        float kpl = (float) (mpg* (1.609344 / 4.54609188));
+        kpl = (float) (Math.round(kpl * 100.0) / 100.0);
+        return kpl;
     }
 
     @Override
