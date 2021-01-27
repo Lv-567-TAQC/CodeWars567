a196f11b08ebdc514682a45a8b3515452e2680c1
Nnn7 <nsteronua@gmail.com>
2021-01-27 13:42:15 +0200

    Some small changes

--- src/main/java/com/ss/ita/kata/Implementation/EightImpl.java
+++ src/main/java/com/ss/ita/kata/Implementation/EightImpl.java
@@ -12,17 +12,15 @@ public class EightImpl implements Eight{
 
 	@Override
 	public double getVolumeOfCuboid(double length, double width, double height) {
-		// TODO Auto-generated method stub
-		return 0;
+		double volumeofcuboid = length *width *height;
+		int result=(int)volumeofcuboid; 
+		return result;
 	}
 
 	@Override
 	public float mpgToKPM(float mpg) {
-		double kmperlitres;
-		double changer = 1.609344d/ 4.54609188;
-		kmperlitres= mpg*changer;
-   		double result = Math.round(kmperlitres*100)/100.00;
-    	return  (float)result;
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
