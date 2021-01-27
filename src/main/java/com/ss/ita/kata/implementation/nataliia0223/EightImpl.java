c34acfacd5d935b5cbc30d58c6862230b16ce746
Nataliia Koval <natalia.koval23@gmail.com>
2021-01-27 21:00:48 +0200

    nataliia0223/ToSquareRootOrNoToSquare

--- src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
@@ -24,7 +24,16 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        for(int i = 0; i <= array.length - 1; i++) {
+            if (array[i] > 0) {
+                if (Math.sqrt(array[i]) == (int) Math.sqrt(array[i])) {
+                    array[i] = (int) Math.sqrt(array[i]);
+                } else {
+                    array[i] *= array[i];
+                }
+            }
+        }
+        return array;
     }
 
     @Override
