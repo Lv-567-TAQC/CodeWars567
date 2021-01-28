dc46bdacca7cd9038fd030aa3e7b6d04273a866e
nataliia0223 <69862777+nataliia0223@users.noreply.github.com>
2021-01-28 17:31:21 +0200

    nataliia0223/KeepHydrated (#122)

---
+++ src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
@@ -0,0 +1,52 @@
+package com.ss.ita.kata.implementation.nataliia0223;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+
+    @Override
+    public int liters(double time) {
+        int liters = (int) time / 2;
+        return liters;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
