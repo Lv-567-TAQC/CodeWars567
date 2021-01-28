6ff400a223558bd41d2940725da508789d408911
c1nnamon-ro11 <vladislaw.vlad2@gmail.com>
2021-01-28 11:34:26 +0200

    c1nnamon-ro11/Artificial Rain

---
+++ src/main/java/com/ss/ita/kata/implementation/vladislavshevhcuk/FiveImp.java
@@ -0,0 +1,66 @@
+package com.ss.ita.kata.implementation.vladislavshevhcuk;
+
+import com.ss.ita.kata.Five;
+
+import java.math.BigInteger;
+
+public class FiveImp implements Five {
+    @Override
+    public int artificialRain(int[] v) {
+        int maxWateredSections =0;
+
+        int numberOfSections = v.length;
+        int[] leftPartOfWateredSection = new int[numberOfSections];
+        int[] rightPartOfWateredSection = new int[numberOfSections];
+
+        //check Left
+        for (int i=1;i<numberOfSections;i++){
+            if(v[i]>=v[i-1]){ //поточне більше за попереднє(лівіше)
+                leftPartOfWateredSection[i]=leftPartOfWateredSection[i-1]+1;
+            }
+            else {
+                leftPartOfWateredSection[i]=0;
+            }
+        }
+
+        //check Right
+        for (int i=v.length-2;i>=0;i--){
+            if(v[i]>=v[i+1]){ //поточне більше за попереднє(правіше)
+                rightPartOfWateredSection[i]=rightPartOfWateredSection[i+1]+1;
+            }
+            else {
+                rightPartOfWateredSection[i]=0;
+            }
+        }
+
+        for(int i=0;i<v.length;i++){
+            maxWateredSections = Math.max(maxWateredSections,leftPartOfWateredSection[i]+rightPartOfWateredSection[i]);
+        }
+        return maxWateredSections+1;
+    }
+
+    @Override
+    public long[] gap(int g, long m, long n) {
+        return new long[0];
+    }
+
+    @Override
+    public int zeros(int n) {
+        return 0;
+    }
+
+    @Override
+    public BigInteger perimeter(BigInteger n) {
+        return null;
+    }
+
+    @Override
+    public double solveSum(double m) {
+        return 0;
+    }
+
+    @Override
+    public long[] smallest(long n) {
+        return new long[0];
+    }
+}
