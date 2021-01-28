02c8ca20ca16d2fccfbfedbbf30aaef856dedc24
Nataliia Koval <natalia.koval23@gmail.com>
2021-01-28 22:37:32 +0200

    nataliia0223/HelpTheBookSeller

--- src/main/java/com/ss/ita/kata/implementation/nataliia0223/SixImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/nataliia0223/SixImpl.java
@@ -46,6 +46,20 @@ public class SixImpl implements Six {
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        String result = "";
+        String letter = "";
+        for (int i = 0; i < lstOf1stLetter.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < lstOfArt.length; j++) {
+                if (lstOfArt[j].charAt(0) == lstOf1stLetter[i].charAt(0)) {
+                    sum += Integer.valueOf(lstOfArt[j].substring(5));
+                    letter = lstOf1stLetter[i];
+                }
+            }
+            result += "(" + letter + " : " + sum + ")";
+        }
+        return result;
     }
+
+
 }
