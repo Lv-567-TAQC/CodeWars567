496e942b0d9c84dd03b9933dbb9f0d4b450ebf26
Kuderiavets-Nata <73226595+Kuderiavets-Nata@users.noreply.github.com>
2021-01-29 11:29:10 +0200

    Merge remote-tracking branch 'origin/main' into kuderiavetsnata/KeepHydrated
    # Conflicts:
    #	src/main/java/com/ss/ita/kata/implementation/andriikapustiak/EightImpl.java

---
+++ src/main/java/com/ss/ita/kata/implementation/kuderiavetsnata/SevenImpl.java
@@ -0,0 +1,30 @@
+package com.ss.ita.kata.implementation.kuderiavetsnata;
+
+import com.ss.ita.kata.Seven;
+
+public class SevenImpl implements Seven {
+
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) sum += arr[i];
+        long ans = (long) Math.ceil((arr.length + 1) * navg - sum);
+        if (ans < 0) throw new IllegalArgumentException();
+        return ans;
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        double sum = 0.0;
+        for (int i = 0; i < n; i++)
+            sum += 1.0 / (1 + 3 * i);
+
+
+        return String.format("%.2f", sum);
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return Math.min(p - bef, aft + 1);
+    }
+}
