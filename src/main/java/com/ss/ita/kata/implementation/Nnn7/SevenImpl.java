2f21eab5797eaa614f9ccf03c169b9c63a96adfd
Nnn7 <nsteronua@gmail.com>
2021-02-04 21:01:09 +0200

    bug fix

--- src/main/java/com/ss/ita/kata/implementation/Nnn7/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/Nnn7/SevenImpl.java
@@ -6,20 +6,33 @@ public class SevenImpl implements Seven{
 
 	@Override
 	public long newAvg(double[] arr, double navg) {
-		int len = arr.length;
-        double sum = 0;
-        long result = 0;
-          
-        if ((navg<30)||(arr[len-1]<0)){        
-            throw new java.lang.IllegalArgumentException();
-        }         
-      
-        for (int i=0; i<len; i++){
-            sum += arr[i];          
-        }  
-        result = (long)((len+1)*navg-sum);
-      
-        return result;
+		  int len = arr.length;
+	        double sum = 0;
+	        long result = 0;      
+	        double test = 0;
+	        if(len==0){
+	            result = (long)navg;
+	            return result;
+	        }
+	      
+	        if ((navg<30)||(arr[len-1]<0)){        
+	            throw new java.lang.IllegalArgumentException();
+	        }         
+	      
+	        for (int i=0; i<len; i++){
+	            sum += arr[i];          
+	        }  
+	        test = ((len+1)*navg-sum);
+	        result=(long)test;
+	        
+	        if(result<0){
+	            throw new java.lang.IllegalArgumentException();
+	        }
+	        
+	        if(test*100%100>0){
+	            result++;
+	        }
+	        return result;
 	}
 
 	@Override
