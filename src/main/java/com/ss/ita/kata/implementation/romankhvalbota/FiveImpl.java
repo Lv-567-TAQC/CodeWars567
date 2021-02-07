4116629dcd64960ee5c60659cc4001ae4a5dd286
Roman-TAQC <roman.it@gmail.com>
2021-02-07 22:25:55 +0200

    Added 'Find the Smallest'

--- src/main/java/com/ss/ita/kata/implementation/romankhvalbota/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/romankhvalbota/FiveImpl.java
@@ -71,6 +71,26 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        long[] arr = new long[3];
+        long min = n;
+        String str = n + "";
+
+        for(int i = 0; i < str.length(); i++){
+            for(int j = 0; j < str.length(); j++){
+                StringBuffer s = new StringBuffer(str);
+                char ch = s.charAt(i);
+                s.deleteCharAt(i);
+                s.insert(j, ch);
+                long num = Long.valueOf(s.toString());
+
+                if(num < min){
+                    min = num;
+                    arr[0] = min;
+                    arr[1] = i;
+                    arr[2] = j;
+                }
+            }
+        }
+        return arr;
     }
 }
