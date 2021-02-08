057f8394f13e959978222335af4cd9d47553b7c3
Khrystyna Shalavylo <shalavilo52@gmail.com>
2021-02-08 17:28:50 +0200

    updated rainfall

--- src/main/java/com/ss/ita/kata/implementation/khrystiash/SixImplementation.java
+++ src/main/java/com/ss/ita/kata/implementation/khrystiash/SixImplementation.java
@@ -25,49 +25,57 @@ public class SixImplementation implements Six {
 
     @Override
     public double mean(String town, String strng) {
-        String[] arrayStrng;
-        Double result = 0.0;
+        String[] data;
+        Double sum = 0.0;
         if (town == ""){
             return -1;
         }
-        arrayStrng = strng.split("\n");
-        for (int i = 0; i < arrayStrng.length;i++){
-            if(town.equals(arrayStrng[i].substring(0,town.length()))){
-                strng = arrayStrng[i].substring(town.length()+1);
+        data = strng.split("\n");
+        for (int i=0; i<data.length;i++){
+            if(town.equals(data[i].substring(0,town.length()))){
+                strng = data[i].substring(town.length()+1);
                 break;
             }
         }
-        arrayStrng = strng.split(" ");
-        Double[] numbs = new Double[arrayStrng.length];
-        for (int i=0; i< arrayStrng.length;i++){
-            numbs[i]=Double.parseDouble(arrayStrng[i]);
-            result+=numbs[i];
+        data = strng.split(",");
+        String[] values = new String[data.length];
+        for (int i=0;i<data.length;i++){
+            values[i]=data[i].substring(4);
         }
-        result = result/12;
-        return result;
+        Double[] numbs = new Double[values.length];
+        for (int i=0; i< values.length;i++){
+            numbs[i]=Double.parseDouble(values[i]);
+            sum+=numbs[i];
+        }
+        sum = sum/12;
+        return sum;
     }
 
     @Override
     public double variance(String town, String strng) {
-        String[] arrayStrng;
+        String[] data;
+        Double sum = 0.0;
         if (town == ""){
             return -1;
         }
-        arrayStrng = strng.split("\n");
-        for (int i=0; i<arrayStrng.length;i++){
-            if(town.equals(arrayStrng[i].substring(0,town.length()))){
-                strng = arrayStrng[i].substring(town.length()+1);
+        data = strng.split("\n");
+        for (int i=0; i<data.length;i++){
+            if(town.equals(data[i].substring(0,town.length()))){
+                strng = data[i].substring(town.length()+1);
                 break;
             }
         }
-        arrayStrng = strng.split(" ");
-        Double[] numbs = new Double[arrayStrng.length];
-        Double sum = 0.0;
-        for (int i=0; i< arrayStrng.length;i++){
-            numbs[i]=Double.parseDouble(arrayStrng[i]);
+        data = strng.split(",");
+        String[] values = new String[data.length];
+        for (int i=0;i<data.length;i++){
+            values[i]=data[i].substring(4);
+        }
+        Double[] numbs = new Double[values.length];
+        for (int i=0; i< values.length;i++){
+            numbs[i]=Double.parseDouble(values[i]);
             sum+=numbs[i];
         }
-        sum = sum/12;
+        sum/=12;
         double result = 0;
         for (int i=0; i< numbs.length;i++){
             numbs[i]=Math.pow((sum - numbs[i]),2);
