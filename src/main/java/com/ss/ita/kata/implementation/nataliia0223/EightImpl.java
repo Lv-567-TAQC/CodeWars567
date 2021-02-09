4f749590184f6ac8a6de24af609f3b68a55f073f
Nataliia Koval <natalia.koval23@gmail.com>
2021-02-09 20:48:53 +0200

    update mpgToKMP

--- src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/nataliia0223/EightImpl.java
@@ -17,8 +17,11 @@ public class EightImpl implements Eight {
 
     @Override
     public float mpgToKPM(float mpg) {
-        float kpl = (float)Math.round(100 * 1.609344 / (float)4.54609188 * mpg) / 100;
-        return kpl;
+        if(mpg >= 0) {
+            float kpl = (float) Math.round(100 * 1.609344 / (float) 4.54609188 * mpg) / 100;
+            return kpl;
+        }
+        return -1;
     }
 
     @Override
