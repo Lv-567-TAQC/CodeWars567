faddfd6fb43db7debb882265cd56332ef621c2e7
c1nnamon-ro11 <vladislaw.vlad2@gmail.com>
2021-02-10 13:07:46 +0200

    DataProvider example

--- src/test/java/com/ss/ita/kata/eigth/ConvertStringToANumberTest.java
+++ src/test/java/com/ss/ita/kata/eigth/ConvertStringToANumberTest.java
@@ -1,76 +1,81 @@
-package java.com.ss.ita.kata.eigth;
+package com.ss.ita.kata.eigth;
 
 
 import com.ss.ita.kata.Eight;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(Parameterized.class)
 public class ConvertStringToANumberTest {
-    List <Eight> eightImpl = Arrays.asList(
-            new com.ss.ita.kata.implementation.andriikapustiak.EightImpl(),
-            new com.ss.ita.kata.implementation.khrystiash.EightImplementation(),
-            new com.ss.ita.kata.implementation.kuderiavetsnata.EightImpl(),
-            new com.ss.ita.kata.implementation.nataliia0223.EightImpl(),
-            new com.ss.ita.kata.implementation.Nnn7.EightImpl(),
-            new com.ss.ita.kata.implementation.romankhvalbota.EightImpl(),
-            new com.ss.ita.kata.implementation.vladislavshevhcuk.EightImp());
+
+    private  final Eight implementation;
+
+    @Parameterized.Parameters(name = "DataProvider {0}")
+    public static Collection<Eight> versionImplementation(){
+        List <Eight> eightImpl = Arrays.asList(
+                new com.ss.ita.kata.implementation.andriikapustiak.EightImpl(),
+                new com.ss.ita.kata.implementation.khrystiash.EightImplementation(),
+                new com.ss.ita.kata.implementation.kuderiavetsnata.EightImpl(),
+                new com.ss.ita.kata.implementation.nataliia0223.EightImpl(),
+                new com.ss.ita.kata.implementation.Nnn7.EightImpl(),
+                new com.ss.ita.kata.implementation.romankhvalbota.EightImpl(),
+                new com.ss.ita.kata.implementation.vladislavshevhcuk.EightImp());
+        return eightImpl;
+    }
+
+    public ConvertStringToANumberTest(Eight implementation){
+        this.implementation = implementation;
+    }
 
     @Test
     public void StringToANumberTest1() {
-        for (Eight impl : eightImpl) {
             int expected = -7;
-            int actual = impl.stringToNumber("-7");
+            int actual = implementation.stringToNumber("-7");
             assertEquals(expected, actual);
-        }
     }
 
     @Test
     public void StringToANumberTest2() {
-        for (Eight impl : eightImpl) {
             int expected = 0;
-            int actual = impl.stringToNumber("0");
+            int actual = implementation.stringToNumber("0");
             assertEquals(expected, actual);
-        }
     }
 
     @Test
     public void StringToANumberTest3() {
-        for (Eight impl : eightImpl) {
             int expected = 1;
-            int actual = impl.stringToNumber("1");
+            int actual = implementation.stringToNumber("1");
             assertEquals(expected, actual);
-        }
     }
 
     @Test
     public void StringToANumberTest4() {
-        for (Eight impl : eightImpl) {
             int expected = 127;
-            int actual = impl.stringToNumber("127");
+            int actual = implementation.stringToNumber("127");
             assertEquals(expected, actual);
-        }
     }
 
     @Test
     public void StringToANumberTest5() {
-        for (Eight impl : eightImpl) {
             int expected = 2147483647;
-            int actual = impl.stringToNumber("2147483647");
+            int actual = implementation.stringToNumber("2147483647");
             assertEquals(expected, actual);
-        }
     }
 
     @Test
     public void StringToANumberTest6() {
-        for (Eight impl : eightImpl) {
             int expected = -2147483648;
-            int actual = impl.stringToNumber("-2147483648");
+            int actual = implementation.stringToNumber("-2147483648");
             assertEquals(expected, actual);
-        }
     }
 
     //assertEquals(false, impl.stringToNumber("Seven"));??
