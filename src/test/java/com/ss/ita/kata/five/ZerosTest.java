a7f7d98b0e0e95b0b6f5e0668c1a75f76f3aebb7
Nataliia Koval <natalia.koval23@gmail.com>
2021-02-10 19:02:18 +0200

    DataProvisers

--- src/test/java/com/ss/ita/kata/five/ZerosTest.java
+++ src/test/java/com/ss/ita/kata/five/ZerosTest.java
@@ -3,49 +3,43 @@ package com.ss.ita.kata.five;
 
 import com.ss.ita.kata.Five;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.*;
 
 
 import static org.junit.Assert.*;
+@RunWith(Parameterized.class)
+public class ZerosTest extends com.ss.ita.kata.five.FiveDataProviderImplementation{
+    private final Five implementation;
 
-public class ZerosTest {
-    List<Five> impl = Arrays.asList(
-            new com.ss.ita.kata.implementation.Nnn7.FiveImpl(),
-         //   new com.ss.ita.kata.implementation.andriikapustiak.FiveImpl(),
-            new com.ss.ita.kata.implementation.khrystiash.FiveImplementation(),
-            new com.ss.ita.kata.implementation.kuderiavetsnata.FiveImpl(),
-            new com.ss.ita.kata.implementation.nataliia0223.FiveImpl(),
-            new com.ss.ita.kata.implementation.romankhvalbota.FiveImpl(),
-            new com.ss.ita.kata.implementation.vladislavshevhcuk.FiveImp());
+    public ZerosTest(final Five implementation) {
+        this.implementation = implementation;
+    }
+
 
     @Test
     public void testZeros1() {
-        for (Five impl : impl) {
-            System.out.println(String.format("Checking for implementation: %s", impl));
-            int actual = impl.zeros(0);
+            System.out.println(String.format("Checking for implementation:" + implementation));
+            int actual = implementation.zeros(0);
             int expected = 0;
             assertEquals(actual, expected);
-        }
     }
 
     @Test
     public void testZeros2() {
-        for (Five impl : impl) {
-            System.out.println(String.format("Checking for implementation: %s", impl));
-            int actual = impl.zeros(6);
+        System.out.println(String.format("Checking for implementation:" + implementation));
+            int actual = implementation.zeros(6);
             int expected = 1;
             assertEquals(actual, expected);
-        }
     }
 
     @Test
     public void testZeros3() {
-        for (Five impl : impl) {
-            System.out.println(String.format("Checking for implementation: %s", impl));
-            int actual = impl.zeros(14);
+            System.out.println(String.format("Checking for implementation:" + implementation));
+            int actual = implementation.zeros(14);
             int expected = 2;
             assertEquals(actual, expected);
-        }
     }
 }
