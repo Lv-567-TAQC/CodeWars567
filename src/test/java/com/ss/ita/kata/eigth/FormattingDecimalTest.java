ae0317d6552807ec5c9cc5276f82298dde1cb6e3
Kuderiavets-Nata <73226595+Kuderiavets-Nata@users.noreply.github.com>
2021-02-10 22:18:31 +0200

    test update

---
+++ src/test/java/com/ss/ita/kata/eigth/FormattingDecimalTest.java
@@ -0,0 +1,35 @@
+package com.ss.ita.kata.eigth;
+
+import com.ss.ita.kata.Eight;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(Parameterized.class)
+public class FormattingDecimalTest extends EightDataProviderImplementation{
+
+    private final Eight implementation;
+
+    public FormattingDecimalTest(Eight implementation) {
+        this.implementation = implementation;
+    }
+
+    @Test
+    public void FormattingDecimal1() {
+        assertEquals(1234.23, implementation.twoDecimalPlaces(1234.23456));
+    }
+
+    @Test
+    public void FormattingDecimalNegativeNumb() {
+        assertEquals(-5.35, implementation.twoDecimalPlaces(-5.3456));
+    }
+
+    @Test
+    public void FormattingDecimalZero() {
+        assertEquals(0.0, implementation.twoDecimalPlaces(0));
+    }
+
+}
+
