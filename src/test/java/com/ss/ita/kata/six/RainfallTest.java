5e167f9d1bbfa8fa51fc12027450c0c0e5906e59
c1nnamon-ro11 <vladislaw.vlad2@gmail.com>
2021-02-16 11:39:39 +0200

    Rename files

--- src/test/java/com/ss/ita/kata/six/RainfallTest.java
+++ src/test/java/com/ss/ita/kata/six/RainfallTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
-public class Rainfall extends SixDataProviderImplementation {
+public class RainfallTest extends SixDataProviderImplementation {
 
     private static String data =
             "Rome:Jan 81.2,Feb 63.2,Mar 70.3,Apr 55.7,May 53.0,Jun 36.4,Jul 17.5,Aug 27.5,Sep 60.9,Oct 117.7,Nov 111.0,Dec 97.9" +
@@ -34,7 +34,7 @@ public class Rainfall extends SixDataProviderImplementation {
 
     private final Six implementation;
 
-    public Rainfall(final Six implementation) {
+    public RainfallTest(final Six implementation) {
         this.implementation = implementation;
     }
 
